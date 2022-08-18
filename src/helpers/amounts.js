02169cac4431fcd29c9caa22a78c6eccc063179f
maxime ngoe <maxime.ngoe@gmail.com>
2022-08-18 12:41:04 +0100

    ajustement amount

--- src/helpers/amounts.js
+++ src/helpers/amounts.js
@@ -6,60 +6,62 @@ export function claimedAmount(r) {
     if ('item' in r){
       return !!r.qtyProvided && !!r.priceAsked ? r.qtyProvided * parseFloat(r.priceAsked) : 0;
     }else{
-      if(Object.keys(r.service).length!=0){
-        let currentPackageType = r.service.packagetype;
-        if(currentPackageType=="S"){
-          totalPrice += parseFloat(r.service.price);
-        }else{
-          // if this product has subItems we had everything
-          if(r.service.serviceserviceSet){
-            r.service.serviceserviceSet.forEach(subItem => {
-              let qtyAsked = 0;
-              if(currentPackageType=="F"){
-                // A ajouter : Verifier que la quantité entré n'est pas superieur a la quantité max
-                // Si c'est le cas mettre la quantité max
-                if(subItem.qtyAsked){
-                  qtyAsked = subItem.qtyAsked;
-                }
-                totalPrice += qtyAsked * subItem.priceAsked;
-              }else if (currentPackageType=="P"){
-                // A ajouter : Si la quantite saisie est superieur a la quantite2 max
-                // reprendre la quantité saisi, sinon reprendre la quantité saisie
-                //console.log(subItem);
-                if(subItem.qtyAsked){
-                  qtyAsked = subItem.qtyAsked;
-                  if(subItem.qtyProvided<subItem.qtyAsked){
-                    qtyAsked = subItem.qtyProvided;
+      if(r.service){
+        if(Object.keys(r.service).length!=0){
+          let currentPackageType = r.service.packagetype;
+          if(currentPackageType=="S"){
+            totalPrice += parseFloat(r.service.price);
+          }else{
+            // if this product has subItems we had everything
+            if(r.service.serviceserviceSet){
+              r.service.serviceserviceSet.forEach(subItem => {
+                let qtyAsked = 0;
+                if(currentPackageType=="F"){
+                  // A ajouter : Verifier que la quantité entré n'est pas superieur a la quantité max
+                  // Si c'est le cas mettre la quantité max
+                  if(subItem.qtyAsked){
+                    qtyAsked = subItem.qtyAsked;
                   }
-                }
-                totalPrice += qtyAsked * subItem.priceAsked;
-              }
-            });
-          }
-          if(r.service.servicesLinked){
-            r.service.servicesLinked.forEach(subItem => {
-              let qtyAsked = 0;
-              if(currentPackageType=="F"){
-                if(subItem.qtyAsked){
-                  qtyAsked = subItem.qtyAsked;
-                }
-                totalPrice += qtyAsked * subItem.priceAsked;
-              }else if (currentPackageType=="P"){
-                if(subItem.qtyAsked){
-                  qtyAsked = subItem.qtyAsked;
-                  if(subItem.qtyProvided<subItem.qtyAsked){
-                    qtyAsked = subItem.qtyProvided;
+                  totalPrice += qtyAsked * subItem.priceAsked;
+                }else if (currentPackageType=="P"){
+                  // A ajouter : Si la quantite saisie est superieur a la quantite2 max
+                  // reprendre la quantité saisi, sinon reprendre la quantité saisie
+                  //console.log(subItem);
+                  if(subItem.qtyAsked){
+                    qtyAsked = subItem.qtyAsked;
+                    if(subItem.qtyProvided<subItem.qtyAsked){
+                      qtyAsked = subItem.qtyProvided;
+                    }
                   }
+                  totalPrice += qtyAsked * subItem.priceAsked;
                 }
-                totalPrice += qtyAsked * subItem.priceAsked;
-              }
-            });
+              });
+            }
+            if(r.service.servicesLinked){
+              r.service.servicesLinked.forEach(subItem => {
+                let qtyAsked = 0;
+                if(currentPackageType=="F"){
+                  if(subItem.qtyAsked){
+                    qtyAsked = subItem.qtyAsked;
+                  }
+                  totalPrice += qtyAsked * subItem.priceAsked;
+                }else if (currentPackageType=="P"){
+                  if(subItem.qtyAsked){
+                    qtyAsked = subItem.qtyAsked;
+                    if(subItem.qtyProvided<subItem.qtyAsked){
+                      qtyAsked = subItem.qtyProvided;
+                    }
+                  }
+                  totalPrice += qtyAsked * subItem.priceAsked;
+                }
+              });
+            }
           }
-        }
 
-        //console.log("totalPrice");
-        //console.log(totalPrice);
-        return totalPrice;  
+          //console.log("totalPrice");
+          //console.log(totalPrice);
+          return totalPrice;  
+        }
       }
     }
   }
