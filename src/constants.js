0150118dbfd2ab6c20ee58872fb99f84fc9c4bfc
michel-lewis <84686058+michel-lewis@users.noreply.github.com>
2025-04-29 11:55:36 +0100

    Update constants.js ADD PATIENT CONDITION

--- src/constants.js
+++ src/constants.js
@@ -73,3 +73,4 @@ export const PRIMARY_OPERATIONAL_INDICATORS_REPORT_QUARTERS = [
 export const STORAGE_KEY_CLAIM_HEALTH_FACILITY = 'claimHealthFacility';
 export const STORAGE_KEY_ADMIN = 'admin';
 export const REFERRAL = "R";
+export const PATIENT_CONDITION = [HEALED, DECEASED, ESCAPED, REFERRAL];
