62a0fb3f19a404016aaea23bf0d8c96260bd1075
michel-lewis <84686058+michel-lewis@users.noreply.github.com>
2025-04-29 11:58:32 +0100

    Update ClaimMasterPanel.js

--- src/components/ClaimMasterPanel.js
+++ src/components/ClaimMasterPanel.js
@@ -133,7 +133,6 @@ class ClaimMasterPanel extends FormPanel {
       isRestored,
       isDuplicate,
     } = this.props;
-    console.log("patient condition config", this.showPatientCondition)
     if (!edited) return null;
     let totalClaimed = 0;
     let totalApproved = 0;
