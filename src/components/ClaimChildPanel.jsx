02d8500985f13cbb7e14e7de932d0b31a4f8bb9f
vaneck237 <vaneckbilongo@gmail.com>
2026-07-09 11:23:24 +0100

    fix sonarcloud signalment

--- src/components/ClaimChildPanel.jsx
+++ src/components/ClaimChildPanel.jsx
@@ -471,7 +471,7 @@ class ClaimChildPanel extends Component {
             <TableCell>
               <NumberInput
                 readOnly={readOnly}
-                value={!!u.qtyAdjusted ? u.qtyAdjusted : u.qtyDisplayed}
+                value={u.qtyAdjusted != null ? u.qtyAdjusted : u.qtyDisplayed}
                 onChange={(v) => {
                   if (!i.service.manualPrice) {
                     if (i.service.packagetype == SERVICE_TYPE_PP_F) {
@@ -552,7 +552,7 @@ class ClaimChildPanel extends Component {
                 <TableCell>
                   <NumberInput
                     readOnly={readOnly}
-                    value={!!u.qtyAdjusted ? u.qtyAdjusted : u.qtyDisplayed}
+                    value={u.qtyAdjusted != null ? u.qtyAdjusted : u.qtyDisplayed}
                     onChange={(v) => {
                       if (!i.service.manualPrice) {
                         if (i.service.packagetype == SERVICE_TYPE_PP_F) {
